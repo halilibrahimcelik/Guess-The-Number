b9c64d337e75ecfba85376351b3423a1597d1fd6
halilibrahimcelik <you@example.com>
2022-06-05 20:27:47 +0300

    Adding input field validators.

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 let localNumber = Math.floor(Math.random() * 100);
 
+const btn = document.querySelector("button");
 const inputArea = document.querySelector(".inputArea");
 const lowerNumber = document.querySelector(".lowerNumber");
 const higherNumber = document.querySelector(".higherNumber");
+const chanceNumber = document.querySelector("strong");
 
 localNumber = 35;
 let enteredNumber = 20;
-let i = 0;
+let count = 5;
 console.log(localNumber);
 
+//!validating input
 function validator() {
-  i++;
-  //   if (i === 3) {
-  //     alert("You have failed to save me!");
-  //     return;
-  //   }
-  console.log(i);
+  if (inputArea.value === "") {
+    alert("Please enter a valid number");
+    return;
+  }
+
   if (localNumber > inputArea.value) {
     lowerNumber.innerText = inputArea.value;
     alert("If want to save me, enter a bigger number");
@@ -25,11 +27,25 @@ function validator() {
   } else if (+inputArea.value === localNumber) {
     alert("Thank you Mario!");
   }
+
+  resetCount();
+  if (count < 0) return null;
+  chanceNumber.innerText = count;
+}
+
+//!reset the count
+function resetCount() {
+  count--;
+  if (count === 0) {
+    alert("You have failed to save me!");
+    return;
+  }
 }
 
 //!validating input number
 inputArea.addEventListener("change", () => {
   let v = parseInt(inputArea.value);
+
   if (v < 1) {
     alert("Minumum number is 0");
     inputArea.value = 1;
@@ -40,7 +56,6 @@ inputArea.addEventListener("change", () => {
   }
 });
 
-const btn = document.querySelector("button");
 btn.addEventListener("click", (e) => {
   e.preventDefault();
 
